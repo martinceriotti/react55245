80a8a51028fcf0619ca8e24afef04dbd6352573c
Martin Ceriotti <martinceriotti@gmail.com>
2023-07-23 19:28:15 -0300

    agregado de fetch

--- lapatisserie/src/components/ItemListContainer/ItemListContainer.jsx
+++ lapatisserie/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,44 @@
-import Container from 'react-bootstrap/Container';
+import { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
 
 export const ItemListContainer = (props) => {
-    return(
-        <Container>
-            
-            <h5>Item List Container</h5>
-            <hr />
-            {props.mensaje}
-            <hr />  
-        </Container>
-       
-    )
-}
- 
+  const [cargando, setCargando] = useState([true]);
+  const [datos, setDatos] = useState([]);
+
+  useEffect(() => {
+    setCargando(true);
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((datos) => setDatos(datos))
+      .finally(() => setCargando(false));
+  }, []);
+
+  return (
+    <>
+    <Container className="container  ">
+      <Row className="row inline-block">
+        {cargando ? (
+          <h2>cargando</h2>
+        ) : (
+          datos.map((articulo) => (
+            <Col className="col " key={articulo.id}>
+              <h3 className="text-primary">{articulo.title}</h3>
+              <h4 className="text-secondary">{articulo.category}</h4>
+              <h4> U$D {articulo.price}</h4>
+              <p className="text-secondary   "> {articulo.description}</p>
+              <img
+                src={articulo.image}
+                width={250}
+                height={250}
+                alt={articulo.title}
+              ></img>
+            </Col>
+          ))
+        )}
+      </Row>
+    </Container>
+    </>
+  );
+};
